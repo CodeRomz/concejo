5ad9c21d3db20a12c3c1fcf6a61f4eb8f3f978a5
CyberRoms <romualdojrb@yahoo.com>
2025-09-09 13:30:03 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,6 @@
     'name': "concejo",
 
     'summary': "Concejo DMS Patch",
-
     'description': """
     """,
 
@@ -14,13 +13,9 @@
     'version': '18.0.1.0.0',
 
     'category': 'Website',
-
-    # any module necessary for this one to work correctly
     'depends': ['website_slides'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/advance_search_templates.xml',
         'views/courses_all_bodyclass.xml',
         'views/res_users.xml'
